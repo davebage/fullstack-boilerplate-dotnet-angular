a474e9ccf90982b729d854787255c5f43f1741b2
Dave Bage <david_bage@hotmail.com>
2023-07-14 16:31:21 +0100

    Implement transactions controller
    - Add not found status for get transaction with Id

---
+++ app-backend/BackEndTests/TransactionsControllerShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using backend.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndTests
+{
+    [TestFixture]
+    public class TransactionsControllerShould
+    {
+        [Test]
+        public void ReturnNotFoundStatusForInvalidTransactionId()
+        {
+            var controller = new TransactionsController();
+
+            var result = controller.GetTransactionForId(Guid.Empty);
+
+            result.Should().BeAssignableTo<NotFoundObjectResult>();
+            ((NotFoundObjectResult)result).Value.Should().Be("Transaction not found");
+        }
+    }
+}
---
+++ app-backend/backend/Controllers/TransactionsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("{transaction_id}")]
+        public IActionResult GetTransactionForId(Guid transaction_id)
+        {
+            return NotFound("Transaction not found");
+        }
+    }
+}
