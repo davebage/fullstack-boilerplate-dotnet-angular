2ecc18372bf673447fc8bc02be1652aee6496520
Dave Bage <david.bage@codurance.com>
2023-07-15 00:47:54 +0100

    update bindings on TransactionRequest

--- app-backend/backend/Controllers/TransactionRequest.cs
+++ app-backend/backend/Controllers/TransactionRequest.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Controllers;
 
 public class TransactionRequest
 {
+    [Required]
     public Guid account_id;
+    [Required]
     public int amount;
 }
