004c62a7a0c666c9a17d5feb96148c6f165c4e38
Dave Bage <david.bage@codurance.com>
2023-07-15 01:34:02 +0100

    Create accounts controller
    - Implement unit tests

---
+++ app-backend/BackEndTests/AccountsControllerShould.cs
@@ -0,0 +1,45 @@
+using backend.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace BackEndTests;
+
+[TestFixture]
+public class AccountsControllerShould
+{
+    [Test]
+    public void ReturnNotFoundWhenInvalidAccountIdOnGet()
+    {
+        var repository = new TransactionsRepository(new DateTimeProvider(), new TransactionIdProvider());
+        var accountId = Guid.NewGuid();
+        repository.AddTransaction(accountId, 100);
+        var controller = new AccountsController(repository);
+        var result = controller.GetAccountData(Guid.Empty);
+
+        result.Should().BeAssignableTo<NotFoundObjectResult>();
+        var objectResult = (NotFoundObjectResult) result;
+        objectResult.Value.Should().Be("Account not found");
+    }
+
+    [Test]
+    public void ReturnOkResultWhenValidAccountIdOnGet()
+    {
+        var repository = new TransactionsRepository(new DateTimeProvider(), new TransactionIdProvider());
+        var mockRepository = new Mock<ITransactionsRepository>();
+        var accountId = Guid.NewGuid();
+        const int accountSum = 100;
+        mockRepository.Setup(x => x.GetAllTransactionsForAccount(It.IsAny<Guid>()))
+            .Returns(new Account(accountId, accountSum));
+
+        var controller = new AccountsController(mockRepository.Object);
+        var result = controller.GetAccountData(Guid.Empty);
+        result.Should().BeAssignableTo<OkObjectResult>();
+        var okResultValue = ((OkObjectResult) result).Value as Account;
+
+        okResultValue.Should().NotBeNull();
+        okResultValue.account_id.Should().Be(accountId);
+        okResultValue.balance.Should().Be(accountSum);
+
+    }
+}
