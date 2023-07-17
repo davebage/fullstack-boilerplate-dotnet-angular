d5abc3890de2e2582444cd4c7ff6f414f238ef05
Dave Bage <david.bage@codurance.com>
2023-07-17 22:04:44 +0100

    fix startup due to CORS error

--- app-backend/backend/Startup.cs
+++ app-backend/backend/Startup.cs
@@ -1,3 +1,5 @@
+using backend.Providers;
+using backend.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,11 +17,20 @@ namespace backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<DateTimeProvider>();
+            services.AddTransient<TransactionIdProvider>();
+            services.AddSingleton<ITransactionsRepository, TransactionsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
